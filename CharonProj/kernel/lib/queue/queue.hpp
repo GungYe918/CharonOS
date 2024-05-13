ee5a37b7a82de1a4c2d6bf90233074d9133e53c5
GungYe <public.gungye@gmail.com>
2024-05-14 01:01:16 +0900

    complete docs for queue.hpp

--- CharonProj/kernel/lib/queue/queue.hpp
+++ CharonProj/kernel/lib/queue/queue.hpp
@@ -97,18 +97,39 @@ Error kQueue<T>::Push(const T& value) {
  * @brief Queue에 데이터를 추가하는 함수
  * 
  * 동작방식:
- *  1) 현재 Queue의 용량이 최대 용량과 같다면 
+ *  1) 현재 Queue의 용량이 최대 용량과 같다면 => 용량이 가득 찼다면
  *     kFull 오류를 반환하고 함수를 종료한다.
  * 
- *  2) 
+ *  2) Queue의 쓰기 위치에 새로운 데이터를 저장한다. 
  * 
  *  3) 현재 Queue의 용량을 1 증가시킨다.
  * 
- *  4) 
+ *  4) 쓰기 위치를 다음으로 이동시킨다.
  * 
- *  5) 
+ *  5) 쓰기 위치가 용량을 초과하여 버퍼의 끝에 도달하였을 경우 
+ *     쓰기 위치를 다시 0으로 초기화한다.
  * 
  *  6) kSuccess를 반환하고 함수를 종료한다.
+ * 
+ * 
+ * PLUS:
+ *  5)에서 쓰기 위치를 다시 0으로 초기화 시키는 이유:
+ *      Queue 배열의 시작과 끝을 연결하기 위해서이다. 
+ *      이러한 형태의 Queue 배열을 원형 버퍼라고 부른다.
+ * 
+ *      원형 버퍼는 순환 구조를 가지고 있기 때문에 효율성과
+ *      메모리 관리 면에서 큰 이점이 있다.
+ * 
+ *      1. 메모리 효율성: 
+ *          원형 버퍼는 고정된 크기의 메모리 블록으로 구현되고,
+ *          메모리 재사용이 허용되기 때문에 동적 메모리 할당이
+ *          필요하지 않다. 따라서 메모리 관리에 필요한 오버헤드를 
+ *          줄일 수 있다.
+ * 
+ *      2. 삽입 및 삭제 연산의 속도 증가:
+ *          원형 버퍼는 인덱스를 순환하며 연산을 수행하기 때문에 
+ *          삽입 및 삭제 시간에 상수 시간(O(1))에 수행된다.
+ *          이 때문에 효율적인 작업 처리가 가능해진다.
  * 
 */
 
@@ -117,24 +138,48 @@ Error kQueue<T>::Push(const T& value) {
 template <typename T>
 Error kQueue<T>::Pop() {
     if (count_ == 0) {                          // 1)
-        return MAKE_ERROR(Error::kEmpty);       // 2)
+        return MAKE_ERROR(Error::kEmpty);       
     }
 
-    --count_;                                   // 3)
-    ++read_pos_;                                // 4)
+    --count_;                                   // 2)
+    ++read_pos_;                                // 3)
 
-    if (read_pos_ == capacity_) {               // 5)
+    if (read_pos_ == capacity_) {               // 4)
         read_pos_ = 0;
     }
-    return MAKE_ERROR(Error::kSuccess);         // 6)
+    return MAKE_ERROR(Error::kSuccess);         // 5)
 }
 
 /**
  * @brief Queue에서 가장 처음 추가한 값을 제거하는 함수이다.
  * 
  * 동작방식:
- *  1)  
-*/
+ *  1)  Queue가 비어있다면 Error::kEmpty를 반환하고 
+ *      함수를 종료한다. 
+ * 
+ *  2)  Queue의 현재 용량을 1 감소시킨다. 
+ * 
+ *  3)  Queue의 읽기 위치를 1 증가시킨다.
+ * 
+ *  4)  read_pos를 1 증가시킨 값이 Queue의 최대 용량과 
+ *      같다면 읽기 위치를 0으로 초기화한다. (원형 버퍼)
+ * 
+ *  5)  Error::kSuccess를 반환하고 함수를 종료한다.
+ * 
+ * 
+ *  PLUS:
+ *   Pop()은 진짜로 값에대한 제거를 실행하는가:
+ *      이 파일에 구현된 Pop()은 실재로 값에 대한 제거를 
+ *      수행하지 않는다. 그 이유는 이 Queue가 원형 버퍼로 
+ *      구현되었기 때문에 굳이 값을 제거할 필요 없이 읽기 위치를 
+ *      증가시키는 것 만으로도 제거한 값에 대한 접근을 방지할 수 
+ *      있기 때문이다.
+ * 
+ *      실재로 Pop이 실행된 위치의 데이터는 원형 버퍼에서 제일 
+ *      뒤로 이동한 것처럼 처리되고, 원형 버퍼가 한 번 순환하게 될 때
+ *      Push함수에 의해 다른 값으로 대체되게 된다.
+ *       
+*/  
 
 
 
@@ -142,13 +187,24 @@ template <typename T>
 size_t kQueue<T>::Count() const {
     return count_;
 }
+/**
+ * @brief 현재 Queue의 용량을 반환한다.
+*/
+
 
 template <typename T>
 size_t kQueue<T>::Capacity() const {
     return capacity_;
 }
+/**
+ * @brief Queue의 최대 용량을 반환한다.
+*/
+
 
 template <typename T>
 const T& kQueue<T>::Front() const {
     return data_[read_pos_];
 }
+/**
+ * @brief 현재 Queue에서 가장 선두에 있는 데이터를 반환한다.
+*/
